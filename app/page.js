7ed28c78251e4ce5a23b6c069d56df1635093705
NNobittaa <rajat28012006@gmail.com>
2026-02-23 21:37:28 +0530

    Landing page added

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import Spline from '@splinetool/react-spline/next';
 
 const page = () => {
   return (
-    <div className='h-screen relative
+    <div className='h-screen relative fit-content
     '>
       <Spline
         scene="https://prod.spline.design/nNegvrD05qJqxbqk/scene.splinecode"
       />
-      <div className='absolute top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2 
-                flex gap-8 justify-center items-center w-full'>
+      <div className='absolute top-4/5 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                flex gap-8 justify-center items-center w-full '>
 
         <button className='cursor-pointer px-10 py-3 rounded-full bg-white text-black font-semibold 
                      hover:bg-gray-200 hover:shadow-[0_0_20px_rgba(255,255,255,0.4)] 
@@ -27,6 +27,24 @@ const page = () => {
       <div className='absolute bg-black text-white/60 bottom-5 text-sm right-4 font-extralight px-3 py-2 rounded-2xl'>
         Crafted for Modern Education
       </div>
+      <div className=" max-sm:block hidden absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-lg 
+                flex-col justify-center items-center px-6 py-28
+                bg-neutral-900/40 backdrop-blur-xl shadow-[0_20px_50px_rgba(0,0,0,0.5)] text-center">
+  
+  <div className="absolute w-32 bg-blue-500/20 blur-[60px] rounded-full" />
+
+  <h1 className="text-5xl md:text-5xl font-bold tracking-tight leading-[1.1] text-white">
+    Study <span className="text-blue-400">Smart.</span><br />
+    Track <span className="text-neutral-400">Better.</span><br />
+    <span className="bg-gradient-to-r from-white to-neutral-500 bg-clip-text text-transparent">
+      Achieve More
+    </span>
+  </h1>
+  
+  <p className="mt-4 text-neutral-400 text-sm font-medium tracking-wide uppercase">
+    Powered by AI Intelligence
+  </p>
+</div>
     </div>
   )
 }
